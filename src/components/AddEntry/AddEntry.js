92ca3b72207e9632daebed9951b80a9730c17df2
Luke Peña <luketpena@gmail.com>
2020-01-03 18:20:06 -0600

    Created form and change handler.

--- src/components/AddEntry/AddEntry.js
+++ src/components/AddEntry/AddEntry.js
@@ -1,10 +1,38 @@
 import React, {Component} from 'react';
 
 class AddEntry extends Component {
+
+  state = {
+    name: '',
+    genre: '',
+    date: '',
+    runtime: '',
+  }
+
+  handleChange = (event,prop)=> {
+    this.setState({
+      [prop]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Add Movie</h2>
+        <form>
+          <input type='text' placeholder='Name' value={this.state.name} onChange={(event)=>this.handleChange(event,'name')}/>
+          <select value='Genre'>
+            <option disabled>Genre</option>
+          </select>
+          <label>
+            Release Date:
+            <input type='date' />
+          </label>
+          <label>
+            <input type='number' placeholder='Duration (in minutes)' />
+          </label>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
